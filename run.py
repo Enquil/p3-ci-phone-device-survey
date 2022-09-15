f9ebaf41fd7148f543e862359f743829e530cf45
Jim Olesen <jim.e.olesen@gmail.com>
2022-09-16 00:16:42 +0200

    restructure functions for better flow control, move valuerrors

--- run.py
+++ run.py
@@ -24,7 +24,6 @@ def validate_data(values):
     formatted
     """
 
-    required_values = 5
     try:
         for value in values:
             
@@ -39,32 +38,26 @@ def validate_data(values):
     
 
     
-def android_capture():
+def survey_capture():
     """
     Captures android device survey data
     """
     print("Please enter survey data as follows: 10,10,10,10,10\n"
-                "(Overall) (Design) (Ease of use) (Camera) (Battery) (Same device brands) (Would switch)\n"
-                "\n(Same device brands) and (Would switch) 'booleans' are represented as 1 or 0 depending if yes or no")
+                "(Overall) (Design) (Ease of use) (Camera) (Battery)\n"
+                "\n Input values must be number between 1 and 10")
     survey_str = input("Please input survey data values, "
                     "then press enter: \n")
     survey_data = survey_str.split(",")
-    if len(survey_data) != 6:
-            raise ValueError(
-                f" 5 values required, you provided {len(values)}"
-            )
-    validate_data(survey_data)   
+        
+    if len(survey_data) != 5:
+        print(
+            f" 5 values required, you provided {len(survey_data)}"
+        )
+        survey_capture()
+    else:          
+        validate_data(survey_data)   
 
-#def select_comparison():
-def ask_pass():
-    from secret import pwd as pwd
-    password = input("Input Password: \n")
-    if password == pwd:
-        return True
-    else:
-        print("Invalid")
-        ask_pass()
-          
+#def select_comparison():         
 
        
 def select_function():
@@ -73,8 +66,7 @@ def select_function():
     """
     print("\n Please choose an action:\n"
             "\n 1. Input Android survey data\n"
-            " 2. Input iPhone survey data\n"
-            " 3. Compare survey data\n")
+            " 2. Compare survey data\n")
     action = input("Please input desired action, "
                     "then press enter: \n")    
     if action == "1":
