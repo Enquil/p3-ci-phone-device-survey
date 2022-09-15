3a9873869f043fc30a85522ce89de45bcccbc28a
Jim Olesen <jim.e.olesen@gmail.com>
2022-09-15 07:56:04 +0200

    add select_function, add ask_os, return error msg on wrong input

--- run.py
+++ run.py
@@ -1,3 +1,54 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file("creds.json")
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open("phonedevice_survey_sheet")
+
+
+def ask_os():
+    """
+    ask for operating system of customers phone
+    """
+    print("\n Please choose customer os:\n"
+            "\n 1. Android\n"
+            " 2. iOS")
+    action = input("Please input desired action, "
+                    "then press enter: \n")    
+    if action == "1":
+        print("hello")
+    elif action == "2":
+        print("goodbye")
+    
+        
+def select_function():
+    """
+    Let user select an action
+    """
+    print("\n Please choose an action:\n"
+            "\n 1. Input customer survey data\n"
+            " 2. Compare data\n")
+    action = input("Please input desired action, "
+                    "then press enter: \n")    
+    if action == "1":
+        ask_os()
+    elif action == "2":
+        choose_comparion()
+    else:
+        print("Invalid input, try again")
+    main()
+        
+def main():
+    """
+    Runs all program functions
+    """
+    select_function()
+    
+main()
